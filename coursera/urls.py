dbf27d567714768ff6df03e6b65cf3cf54f74e76
Deusdedit Vilar <deusdedit.vilar@gmail.com>
2022-08-02 19:26:05 -0300

    Second assignment

--- coursera/urls.py
+++ coursera/urls.py
@@ -13,11 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
+from django.urls import include, path, re_path
+# from django.conf.urls import url
+from django.views.static import serve
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SITE_ROOT = os.path.join(BASE_DIR, 'site')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('class.urls')),
+    re_path(r'^site/(?P<path>.*)$', serve,
+        {'document_root': SITE_ROOT, 'show_indexes': True},
+        name='site_path'
+    ),
 ]
